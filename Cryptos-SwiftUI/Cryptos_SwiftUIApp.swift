9d58a18964e49bfbc0c201960003a26ccaea68bb
Necati Alperen IŞIK <necatialperen2@gmail.com>
2024-09-19 00:49:56 +0300

    Initial Commit

---
+++ Cryptos-SwiftUI/Cryptos_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cryptos_SwiftUIApp.swift
+//  Cryptos-SwiftUI
+//
+//  Created by Necati Alperen IŞIK on 19.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct Cryptos_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
